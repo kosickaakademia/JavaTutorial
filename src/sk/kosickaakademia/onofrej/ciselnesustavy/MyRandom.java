b2355a2d0bc066bac4e03f99f7c848cc53f1e169
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2020-10-19 13:58:05 +0200

    Added caesar's cipher, added ASCII

--- src/sk/kosickaakademia/onofrej/ciselnesustavy/MyRandom.java
+++ src/sk/kosickaakademia/onofrej/ciselnesustavy/MyRandom.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class MyRandom {
     public static void main(String[] args) {
         MyRandom myRandom=new MyRandom();
-        myRandom.guessNumber(310);
+        //myRandom.guessNumber(310);
+        //myRandom.vypis();
+        System.out.println(myRandom.caesarsCipher("Dnes je PONDelok 19.10.2020 #!?+"));
     }
 
     public void guessNumber(int max){  // 0,1451236 -> 14,51236
@@ -60,6 +62,94 @@ public class MyRandom {
         System.out.println(c+" "+m);
     }
 
+    public void triangle(){
+        String text="Informatika";
+        int len = text.length();
+            int i,j;
+            for(i=0;i<len;i++){
+                for(j=i;j<len;j++) {
+                    System.out.print(text.charAt(j));
+                }
+            System.out.println();
+        }
+    }
+    public void triangle2(){
+        String text="Informatika";
+        int len = text.length();
+        int i,j;
+        for(i=0;i<len;i++){
+            String myString = text.substring(i);
+            System.out.println(myString);
+        }
+    }
 
+    public void example91(){
+        int x,y;
+        for(x=0;x<=9;x++){
+            for(y=0;y<=9;y++){
+                int value=  807502 + x*10000 + y*10;
+                if(value%91==0)
+                    System.out.println(value);
+            }
+        }
+    }
+
+    public void sumOfFragments(){
+        double p=0;
+        int i;
+        for(i=1;i<=100;i++){
+            p=p+1.0/i;
+        }
+        System.out.println(p);
+    }
+
+    public void printAscii(){
+        int i;
+        for(i=65;i<=90;i++){
+            System.out.println(i+" : " +(char)i + " ");
+        }
+        String r="kosice2021";
+        char[] pole = r.toCharArray();
+        int len = pole.length;
+        for(i=0;i<len;i++)
+            pole[i]=(char)(pole[i]+5);
+        String newR=String.valueOf(pole);
+        System.out.println(newR);
+    }
+
+    public String caesarsCipher(String text){
+        int len = text.length();
+        String sifra="";
+        int i;
+        for(i=0;i<len; i++){
+            char z=text.charAt(i);
+            if(z>='A' && z<='Z'){
+                z=(char)(z+3);
+                if(z>'Z')
+                    z=(char)(z-26);
+            }
+            else if(z>='a' && z<='z'){
+                z=(char)(z+3);
+                if(z>'z')
+                    z=(char)(z-26);
+            }
+            else if(z>='0' && z<='9'){
+                z=(char)(z+3);
+                if(z>'9')
+                    z=(char)(z-10);
+            }
+            sifra=sifra+z;
+        }
+        return sifra;
+    }
+
+    public void textAnalyzer(String text){
+        // vypise:
+        // pocet malych pismen: 85
+        // pocet velkych: 58
+        // pocet cislic : 8
+        // pocet medzier: 17
+        // pocet viet: . ! ?
+    }
 
 }
